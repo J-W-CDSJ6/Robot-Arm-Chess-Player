a482194dd1b458a5fd984b561e2b7fadb87c0f2e
JW <115539379+J-W-CDSJ6@users.noreply.github.com>
2022-11-16 13:55:35 +0800

    Create main.py
    The main control file.

---
+++ main.py
@@ -0,0 +1,16 @@
+class Board:
+  def __init__(self):
+    self.FEN = "8/8/8/8/8/8/8/8 w - - 0 1"
+  
+  def move(self, move):
+    pass
+
+class Game:
+  def __init__(self):
+    self.history = []
+
+def main():
+  pass
+  
+if __name__ == "__main__":
+  main()
