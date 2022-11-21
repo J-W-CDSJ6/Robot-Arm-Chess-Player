e6f5b16e316799f681d088ecf43c7c1ef625f7ed
JW <115539379+J-W-CDSJ6@users.noreply.github.com>
2022-11-21 16:18:27 +0800

    Added getpiece function to Board.

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
+emptyBoardFEN = "8/8/8/8/8/8/8/8 w - - 0 1"
+
+class Move:
+  def __init__(self, moveStr):
+    assert type(moveStr) == str, "Cannot make move from non-string!"
+    self.moveFrom = moveStr[0:1]
+    self.moveTo = moveStr[2:3]
+    self.promotion = moveStr[4] if len(moveStr) > 4 else ""
+
 class Board:
-  def __init__(self):
-    self.FEN = "8/8/8/8/8/8/8/8 w - - 0 1"
+  def __init__(self, FEN="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
+    self.FEN = FEN
+
+  @property
+  def listedBoard(self):
+    listed = self.FEN.split("/")
+    infos = listed[7].split()
+    listed[7] = infos[0]
+    boardedObj = {
+      "board": listed,
+      "turn": infos[5],
+      "player": infos[1],
+      "castles": infos[2],
+      "passantable": infos[3],
+      "half_move": infos[4]
+    }
+    return boardedObj
   
+  def getpiece(self, position):
+    File = ord(position[0])-97
+    Rank = int(position[1])
+    boardObj = self.listedBoard
+    rankStr = boardObj["board"][8-Rank]
+    expandedRankStr = ""
+    for char in rankStr:
+      try:
+        expandedRankStr += int(char)*"1"
+      except:
+        expandedRankStr += char
+    return expandedRankStr[File]
+
   def move(self, move):
     pass
 
@@ -10,7 +47,10 @@ class Game:
     self.history = []
 
 def main():
-  pass
+  empty = Board()
+  print(empty.getpiece("e7"))
+
   
 if __name__ == "__main__":
   main()
+
