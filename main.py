472e487ebf652cbee8c9f09d45c420486fb8bd09
JW <115539379+J-W-CDSJ6@users.noreply.github.com>
2022-11-22 18:28:29 +0800

    Added two COM players

--- main.py
+++ main.py
@@ -16,18 +16,39 @@ def replace_piece_name_with_unicdoe_symbol(vis):
   newvis = newvis.replace("p", u"♟︎")
   return newvis
 
-def make_best_move(instance, time_constraint):
+def make_best_move(instance):
+  move = instance.get_best_move()
+  if move is None: return None
+  instance.make_moves_from_current_position([move])
+  return move
+
+def make_best_move_with_time_constraint(instance, time_constraint=1000):
   move = instance.get_best_move_time(time_constraint)
   if move is None: return None
   instance.make_moves_from_current_position([move])
   return move
 
 def main():
-  instance = Stockfish(path="C:/Users/Administrator/Documents/GitHub/Robot-Arm-Chess-Player/stockfish_15_win_x64_avx2/stockfish_15_x64_avx2.exe")
+  white = Stockfish(path="C:/Users/Administrator/Documents/GitHub/Robot-Arm-Chess-Player/stockfish_15_win_x64_avx2/stockfish_15_x64_avx2.exe")
+  black = Stockfish(path="C:/Users/Administrator/Documents/GitHub/Robot-Arm-Chess-Player/stockfish_15_win_x64_avx2/stockfish_15_x64_avx2.exe")
+  drawing_turns = 0
   while True:
-    vis = replace_piece_name_with_unicdoe_symbol(instance.get_board_visual())
-    print(vis, instance.get_evaluation())
-    if make_best_move(instance, 50) is None: break
+    vis = replace_piece_name_with_unicdoe_symbol(white.get_board_visual())
+    eval = white.get_evaluation()
+    print(vis, eval)
+    if eval["value"] == 0: drawing_turns += 1
+    else: drawing_turns = 0
+    if make_best_move(white) is None: break
+    black.set_fen_position(white.get_fen_position())
+    vis = replace_piece_name_with_unicdoe_symbol(black.get_board_visual())
+    eval = black.get_evaluation()
+    print(vis, eval)
+    if eval["value"] == 0: drawing_turns += 1
+    else: drawing_turns = 0
+    if make_best_move(black) is None: break
+    white.set_fen_position(black.get_fen_position())
+    if drawing_turns > 50: break
+
   print("gg")
   
 if __name__ == "__main__":
